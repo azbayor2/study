be16b73e71ed04b71cd79a0f9b80aec42da4e950
azbayor2 <hanharry2@gmail.com>
2025-09-19 13:20:11 +0900

    3주차 문제 추가

--- baekjoon/Code/goondae/1552.cpp
+++ baekjoon/Code/goondae/1552.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int N, mmax = -100000000, mmin = 100000000;
 int arr[7][7];
 
-void dfs(int start, int cur, int mask, int ans, int grp, bool flag){
-    if(flag==1 && start==cur)
+void dfs(int start, int cur, int mask, int ans, int grp){
+    if(start==cur && (mask & (1<<start))>0)
     {
-        mask = mask | (1<<start); grp++;
+        grp++;
         if(mask == ((1<<N)-1)){
             int temp = (grp%2)==0 ? -ans:ans;
             mmax = max(mmax, temp); mmin = min(mmin, temp);
@@ -17,7 +17,7 @@ void dfs(int start, int cur, int mask, int ans, int grp, bool flag){
         else{
             for(int i =0; i<N; i++){
                 if((mask &(1<<i))==0){
-                    dfs(i, i, mask | (1<<i), ans, grp, 0);
+                    dfs(i, i, mask, ans, grp);
                     break;
                 }
             }
@@ -28,8 +28,8 @@ void dfs(int start, int cur, int mask, int ans, int grp, bool flag){
     
     for(int i = 0; i<N; i++){
         if((mask & (1<<i))!=0) continue;
-        int next_mask = (flag==1) ? mask|(1<<cur):mask;
-        dfs(start, i, next_mask, ans*arr[cur][i], grp, 1);
+        int next_mask = mask|(1<<i);
+        dfs(start, i, next_mask, ans*arr[cur][i], grp);
     }
 
     return;
@@ -47,7 +47,7 @@ int main(){
             if(c>='0' && c<='9') arr[i][j] = (int)(c-'0');
             else arr[i][j] = -((int)(c-'A')+1);
         }
-    dfs(0, 0, 0, 1, 0, 0);
+    dfs(0, 0, 0, 1, 0);
 
     cout << mmin << "\n" << mmax << "\n";
 
